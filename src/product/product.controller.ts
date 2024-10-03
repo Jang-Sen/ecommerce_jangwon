bde2f52e38c3f9f63be64a13e044f265470a757c
jangsen <dh7895210@naver.com>
2024-10-03 17:22:35 +0900

    create & getAll & getById

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  // product 전체 데이터 불러오는 api
+  @Get('/all')
+  async getAllProducts() {
+    return await this.productService.getAllProducts();
+  }
+
+  // product 상세(id) 데이터 불러오는 api
+  @Get('/:productId')
+  async getProductById(@Param('productId') productId: string) {
+    return await this.productService.getProductById(productId);
+  }
+
+  // product 신규 등록 api
+  @Post('/new')
+  async createProduct(
+    // @Body('name') name: string,
+    // @Body('description') description: string,
+    // @Body('price') price: number,
+    // @Body('category') category: string,
+    // @Body('productImg') productImg: string,
+    @Body() createProductDto: CreateProductDto,
+  ) {
+    return await this.productService.createProduct(createProductDto);
+  }
+
+  // product 수정 api
+
+  // id에 해당되는 product 삭제 api
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  async getAllProducts() {
+    return await this.productRepository.find();
+  }
+
+  async getProductById(productId: string) {
+    const product = await this.productRepository.findOneBy({ id: productId });
+    if (!product) {
+      throw new NotFoundException('product not found');
+    }
+
+    return product;
+  }
+
+  async createProduct(createProductDto: CreateProductDto) {
+    const newProduct = await this.productRepository.create(createProductDto);
+    await this.productRepository.save(newProduct);
+
+    return newProduct;
+  }
+}
